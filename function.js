5e6cc42052881947a534a1ae768c5eaf58795b75
feothyuth <129170330+feothyuth@users.noreply.github.com>
2024-02-02 14:57:17 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,15 @@
+import Web3 from 'web3';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const web3 = new Web3('https://polygon-rpc.com');
+const account = process.env.PRIVATE_KEY;
+
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]]; // Swap elements
     }
 }
+
+
